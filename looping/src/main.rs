6e58a898e22a494901204ba41778080090ba2255
jefaye <clement.jean.l.faye@gmail.com>
2024-09-23 15:19:46 +0000

    VARIABLES - 4

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,22 +1,21 @@
 use std::io;
 
 fn main() {
-    let riddle = String::from("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
     let mut input = String::new();
     let answer = String::from("The letter e");
-    let mut tries = 0;
-    
+    let mut trials = 0;
+
     loop {
-        println!("{:?}", &riddle);
-        
+        println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
+
         io::stdin().read_line(&mut input).unwrap();
 
         if input.trim() == &answer {
-            println!("{:?}", tries);
+            println!("Number of trials: {trials}", trials);
             break;
         };
 
-        tries += 1;
+        trials += 1;
         input.clear();
     }
 }
