208762a3a7d6b9abf074ebf9cf53f9e5f6592c44
jefaye <clement.jean.l.faye@gmail.com>
2024-09-24 15:18:12 +0000

    MEMORY - 6

--- ownership/src/lib.rs
+++ ownership/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn first_subword(mut s: String) -> String {
+pub fn first_subword(s: String) -> String {
     for (i, c) in s.chars().enumerate() {
         if i != 0 && (c == '_' || c.is_uppercase()) {
             return s[0..i].to_string();
