7a26e9f20c9be352feac391911ca56e1cabe8c89
jefaye <clement.jean.l.faye@gmail.com>
2024-09-24 10:54:17 +0000

    MEMORY -

---
+++ to_url/src/main.rs
@@ -0,0 +1,10 @@
+use to_url::*;
+
+fn main() {
+	let s = "Hello, world!";
+	println!("{} to be use as an url is {}", s, to_url(s));
+}
+
+// $ cargo run
+// Hello, world! to be use as an url is Hello,%20world!
+// $
