5ac28992f53682c945d2878a93c57be1bbfc5393
jefaye <clement.jean.l.faye@gmail.com>
2024-09-24 11:18:38 +0000

    MEMORY - 4

---
+++ string_literals/src/main.rs
@@ -0,0 +1,17 @@
+use string_literals::*;
+
+fn main() {
+    println!("{}", is_empty(""));
+    println!("{}", is_ascii("rust"));
+    println!("{}", contains("rust", "ru"));
+    println!("{:?}", split_at("rust", 2));
+    println!("{}", find("rust", 'u'));
+}
+
+// $ cargo run
+// true
+// true
+// true
+// ("ru", "st")
+// 1
+// $
