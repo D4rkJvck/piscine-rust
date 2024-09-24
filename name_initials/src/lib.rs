cd7fbd640b268a05e8e8a34692b02043f2adca83
jefaye <clement.jean.l.faye@gmail.com>
2024-09-24 13:51:06 +0000

    MEMORY - 5

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,39 +1,14 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
-    let mut initials: Vec<String> = Vec::new();
-
-    for name in names {
-        let initial = name
-            .split_whitespace()
-            .collect::<Vec<&str>>()
-            .iter()
-            .map(|inits| inits
-                .chars()
-                .next()
-                .unwrap_or_default()
-                .to_string() + ".")
-            .collect::<Vec<String>>()
-            .join(" ");
-
-        initials.push(initial);
-    }
-
-    initials
+    names.iter()
+        .map(|name| {
+            name.split_whitespace()
+                .map(|inits|
+                    inits.chars()
+                    .next()
+                    .unwrap_or_default()
+                    .to_string() + ".")
+                .collect::<Vec<String>>()
+                .join(" ")
+        })
+        .collect()
 }
-
-// pub fn initials(names: Vec<&str>) -> Vec<String> {
-//     names
-//         .iter()
-//         .map(|name| {
-//             name.split_whitespace()
-//                 .collect::<Vec<&str>>()
-//                 .iter()
-//                 .map(|inits| inits
-//                     .chars()
-//                     .next()
-//                     .unwrap_or_default()
-//                     .to_string() + ".")
-//                 .collect::<Vec<String>>()
-//                 .join(" ")
-//         })
-//         .collect()
-// }
