db74ec9e73898b580b6a5fe369097d5fc0bef52f
jefaye <clement.jean.l.faye@gmail.com>
2024-09-24 10:22:27 +0000

    MEMORY - 1

---
+++ borrow/src/main.rs
@@ -0,0 +1,14 @@
+use borrow::*;
+
+fn main() {
+	let s = "hello";
+	let s1 = "camelCase".to_string();
+
+	println!("\tstr_len(\"{}\") = {}", s, str_len(s));
+	println!("\tstr_len(\"{}\") = {}", s1, str_len(&s1));
+}
+
+// $ cargo run
+// str_len("hello") = 5
+// str_len("camelCase") = 9
+// $
