a7889ffce88c31fdbe55a3faec693adff0c0be92
jefaye <clement.jean.l.faye@gmail.com>
2024-09-24 14:13:18 +0000

    MEMORY - 5

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,14 +1,10 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
-    names.iter()
+    names
+        .iter()
         .map(|name| {
             name.split_whitespace()
-                .map(|inits|
-                    inits.chars()
-                    .next()
-                    .unwrap_or_default()
-                    .to_string() + ".")
-                .collect::<Vec<String>>()
-                .join(" ")
+                .map(|inits| format!("{}. ", inits.chars().next().unwrap_or_default()))
+                .collect()
         })
         .collect()
 }
