c210df4205ee8a91133db441daaf15ca73fc26ac
jefaye <clement.jean.l.faye@gmail.com>
2024-09-24 16:54:02 +0000

    MEMORY - 7

---
+++ copy/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn nbr_function(c: i32) -> (i32, f64, f64) {
+    (c, (c as f64).exp(), (c as f64).ln())
+}
+
+pub fn str_function(a: String) -> (String, String) {
+    (
+        a.clone(),
+        a.split_whitespace()
+            .map(|num| num.parse::<f64>()
+            .unwrap_or_default()
+            .exp()
+            .to_string())
+            .collect::<Vec<String>>()
+            .join(" "),
+    )
+}
+
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
+    (b.clone(), b.iter().map(|x| (*x as f64).ln()).collect())
+}
