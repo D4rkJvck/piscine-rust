89d394ee28afa10380b7403f27447073c894fcb0
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-09-25 07:23:10 +0000

    MEMORY - 9

--- tic_tac_toe/src/lib.rs
+++ tic_tac_toe/src/lib.rs
@@ -1,21 +1,34 @@
 pub fn tic_tac_toe(table: Vec<Vec<&str>>) -> String {
-    if horizontal("X", &table) {
-        return String::from("player X won");
+    if horizontal("X", &table) || vertical("X", &table) || diagonals("X", &table) {
+        return String::from("player X won")
+    }
+    
+    if horizontal("O", &table) || vertical("O", &table) || diagonals("O", &table) {
+        return String::from("player O won")
     }
 
-    if vertical("O", &table) {
-        return String::from("player O won");
-    }
-
-    if diagonals("X", &table) {
-        return String::from("player X won");
-    }
-
-    String::new()
+    String::from("tie")
 }
+
+////////////////////////////////////////////////////////////////////////
 
 pub fn diagonals(player: &str, table: &Vec<Vec<&str>>) -> bool {
+    if table[1][1] != player {
+        return false
+    }
+
+    if table[0][0] == player && table[2][2] == player {
+        return  true;
+    }
+    
+    if table[0][2] == player && table[2][0] == player {
+        return true
+    }
+    
+    false
 }
+
+////////////////////////////////////////////////////////////////////////
 
 pub fn horizontal(player: &str, table: &Vec<Vec<&str>>) -> bool {
     for row in table {
@@ -23,9 +36,11 @@ pub fn horizontal(player: &str, table: &Vec<Vec<&str>>) -> bool {
             return true;
         }
     }
-
+    
     false
 }
+
+////////////////////////////////////////////////////////////////////////
 
 pub fn vertical(player: &str, table: &Vec<Vec<&str>>) -> bool {
     for col in 0..table.len() {
