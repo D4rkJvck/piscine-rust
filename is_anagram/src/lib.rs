6e939083d0ffc5501a76508bc2b7c351596a11e5
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-09-26 21:48:05 +0000

    DATA - 13

--- is_anagram/src/lib.rs
+++ is_anagram/src/lib.rs
@@ -1,5 +1,7 @@
 use string_permutation::is_permutation;
 
 pub fn is_anagram(s1: &str, s2: &str) -> bool {
-    is_permutation(s1, s2)
+    let str_a = s1.to_lowercase();
+    let str_b = s2.to_lowercase();
+    is_permutation(str_a.as_str(), str_b.as_str())
 }
