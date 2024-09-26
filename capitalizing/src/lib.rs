65280ea77ee9378d8707d263afe10d8a41fbbc1f
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-09-26 12:31:27 +0000

    DATA - 6

---
+++ capitalizing/src/lib.rs
@@ -0,0 +1,37 @@
+pub fn capitalize_first(input: &str) -> String {
+    input
+        .chars()
+        .next()
+        .unwrap()
+        .to_uppercase()
+        .collect::<String>()
+        + &input[1..]
+}
+
+//________________________________________________________
+//
+
+pub fn title_case(input: &str) -> String {
+    input
+    .split_whitespace()
+    .map(|word| capitalize_first(word))
+    .collect::<Vec<String>>()
+    .join(" ")
+}
+
+//________________________________________________________
+//
+
+pub fn change_case(input: &str) -> String {
+    input
+        .chars()
+        .into_iter()
+        .map(|c| {
+            if c.is_uppercase() {
+                c.to_lowercase().collect::<String>()
+            } else {
+                c.to_uppercase().collect::<String>()
+            }
+        })
+        .collect()
+}
