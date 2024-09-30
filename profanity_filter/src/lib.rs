b55b7c3d87074be2e04cdc6b0536dcf73d172dd3
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-09-30 14:20:28 +0000

    ERRORS - 4

---
+++ profanity_filter/src/lib.rs
@@ -0,0 +1,28 @@
+pub struct Message {
+    content: String,
+    _user: String,
+}
+
+impl Message {
+    pub fn new(content: String, _user: String) -> Self {
+        Message { content, _user }
+    }
+
+    pub fn get_content(&self) -> &str {
+        &self.content
+    }
+
+    pub fn send_ms(&self) -> Option<&str> {
+        match self.get_content() {
+            content if !content.contains("stupid") && content != "" => Some(&content),
+            _ => None,
+        }
+    }
+}
+
+pub fn check_ms(msg: &Message) -> (bool, &str) {
+    match msg.send_ms() {
+        Some(msg) => (true, msg),
+        None => (false, "ERROR: illegal")
+    }
+}
