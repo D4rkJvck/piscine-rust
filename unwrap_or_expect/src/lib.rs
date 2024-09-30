4dbfce1e75070e94414ac8d78b5d0eac24fb76fa
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-09-30 11:38:15 +0000

    ERRORS - 1

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -9,17 +9,39 @@ pub enum Security {
 pub fn fetch_data(server: Result<String, String>, sec_lvl: Security) -> String {
     use self::Security::*;
 
-    match server {
-        Ok(url) => match sec_lvl {
-            BlockServer => panic!("{}", url.to_string()),
-            _ => url.to_string(),
-        },
-        Err(url) => match sec_lvl {
-            Unknown => panic!(),
-            High => panic!("ERROR: program stops"),
-            Medium => String::from("WARNING: check the server"),
-            Low => format!("Not found: {}", url),
-            _ => unreachable!(),
-        },
+    match sec_lvl {
+        Unknown => server.unwrap(),
+        High => server.expect("ERROR: program stops"),
+        Medium => server.unwrap_or("WARNING: check the server".to_string()),
+        Low => server.unwrap_or_else(low_lvl_msg),
+        BlockServer => server.unwrap_err(),
     }
 }
+
+//----------------------------------------------------------------
+
+fn low_lvl_msg(url: String) -> String {
+    format!("Not found: {}", url)
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// pub fn fetch_data(server: Result<String, String>, sec_lvl: Security) -> String {
+//     use self::Security::*;
+
+//     match server {
+//         Ok(url) => match sec_lvl {
+//             BlockServer => panic!("{}", url.to_string()),
+//             _ => url.to_string(),
+//         },
+//         Err(url) => match sec_lvl {
+//             Unknown => panic!(),
+//             High => panic!("ERROR: program stops"),
+//             Medium => String::from("WARNING: check the server"),
+//             Low => format!("Not found: {}", url),
+//             _ => unreachable!(),
+//         },
+//     }
+// }
