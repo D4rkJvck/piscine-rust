586171c848adf490debdee7a95ebead15cc28ba8
jefaye <clement.jean.l.faye@gmail.com>
2024-10-01 17:42:49 +0000

    ERRORS - 10

--- binary_search/src/lib.rs
+++ binary_search/src/lib.rs
@@ -1,4 +1,8 @@
 pub fn binary_search(sorted_list: &[i32], target: i32) -> Option<usize> {
+    if sorted_list.is_empty() {
+        return None;
+    }
+    
     let mut left = 0;
     let mut right = sorted_list.len() - 1;
 
