f1ccb03d430a9b2e335a8dd3d088c358fc71d8fb
jefaye <clement.jean.l.faye@gmail.com>
2024-10-01 21:21:04 +0000

    CREATES - 1

---
+++ middle_day/src/lib.rs
@@ -0,0 +1,17 @@
+use chrono::{Datelike, NaiveDate, Weekday as wd};
+
+pub fn middle_day(year: i32) -> Option<wd> {
+    if is_leap_year(year) {
+        None
+    } else {
+        let date = NaiveDate::from_ymd_opt(year, 7, 2).unwrap();
+        let day = date.weekday();
+        Some(day)
+    }
+}
+
+////////////////////////////////////////////////////////////
+
+fn is_leap_year(year: i32) -> bool {
+    (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+}
---
+++ middle_day/src/main.rs
@@ -0,0 +1,9 @@
+use middle_day::*;
+
+fn main() {
+    println!("{:?}", middle_day(1022).unwrap());
+}
+
+// $ cargo run
+// Tue
+// $
