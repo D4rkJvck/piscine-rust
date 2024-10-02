9efb8d7ddb714ae277976a2f2ffe3668c47e20c1
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-10-02 22:20:37 +0000

    CREATES - 5

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -1,11 +1,10 @@
-pub use case::CaseExt;
+use case;
 pub use edit_distance::edit_distance;
 
 pub fn expected_variable(to_cmp: &str, expected: &str) -> Option<String> {
     if !(is_camel(to_cmp) || is_snake(to_cmp)) {
         return None;
     }
-
 
     let diff = edit_distance(
         to_cmp.to_lowercase().as_str(),
@@ -29,11 +28,11 @@ pub fn expected_variable(to_cmp: &str, expected: &str) -> Option<String> {
 //
 
 fn is_camel(s: &str) -> bool {
-    s.chars().any(|c| c.is_uppercase())
+    s.chars().any(|c| c.is_uppercase()) && !s.contains(' ')
 }
 
 //-----------------------------------------------
 
 fn is_snake(s: &str) -> bool {
-    s.contains('_')
+    s.contains('_') && s.chars().any(|c| c.is_uppercase()) == false
 }
