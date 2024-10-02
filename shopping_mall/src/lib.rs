a9882125ba6abd54dcf9ec4da2777919172208ab
jefaye <clement.jean.l.faye@gmail.com>
2024-10-02 11:54:00 +0000

    CREATES - 4

---
+++ shopping_mall/src/lib.rs
@@ -0,0 +1,27 @@
+pub mod mall;
+
+pub use mall::{
+    floor::store::{employee::Employee, Store},
+    guard::Guard,
+    Mall,
+};
+
+pub fn biggest_store(mall: Mall) -> Store {
+    todo!()
+}
+
+pub fn highest_paid_employee(mall: Mall) -> Employee {
+    todo!()
+}
+
+pub fn nbr_of_employees(mall: Mall) -> usize {
+    todo!()
+}
+
+pub fn check_for_securities(mall: Mall, guards: Vec<Guard>) {
+    todo!()
+}
+
+pub fn cut_or_raise(mall: Mall) {
+    todo!()
+}
