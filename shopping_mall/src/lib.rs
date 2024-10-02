ef0546f87e13c8b888cbbfe9145513fbad6b54d6
jefaye <clement.jean.l.faye@gmail.com>
2024-10-02 12:50:20 +0000

    CREATES - 4: Biggest Store [DONE]

--- shopping_mall/src/lib.rs
+++ shopping_mall/src/lib.rs
@@ -1,27 +1,29 @@
 pub mod mall;
 
-pub use mall::{
-    floor::store::{employee::Employee, Store},
-    guard::Guard,
-    Mall,
-};
+pub use mall::floor::*;
+pub use mall::*;
 
-pub fn biggest_store(mall: Mall) -> Store {
+pub fn biggest_store(mall: mall::Mall) -> mall::floor::store::Store {
+    mall.floors
+        .iter()
+        .flat_map(|f| f.stores.iter())
+        .max_by_key(|s| s.square_meters)
+        .cloned()
+        .unwrap()
+}
+
+pub fn highest_paid_employee(mall: mall::Mall) -> mall::floor::store::employee::Employee {
     todo!()
 }
 
-pub fn highest_paid_employee(mall: Mall) -> Employee {
+pub fn nbr_of_employees(mall: mall::Mall) -> usize {
     todo!()
 }
 
-pub fn nbr_of_employees(mall: Mall) -> usize {
+pub fn check_for_securities(mall: &mall::Mall, guards: Vec<mall::guard::Guard>) {
     todo!()
 }
 
-pub fn check_for_securities(mall: Mall, guards: Vec<Guard>) {
-    todo!()
-}
-
-pub fn cut_or_raise(mall: Mall) {
+pub fn cut_or_raise(mall: &mall::Mall) {
     todo!()
 }
