f556471aa51d8b3c5af6abc0f33828f4d9518dce
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-10-02 16:08:25 +0000

    CREATES - 4

--- shopping_mall/src/lib.rs
+++ shopping_mall/src/lib.rs
@@ -12,14 +12,31 @@ pub fn biggest_store(mall: mall::Mall) -> mall::floor::store::Store {
         .unwrap()
 }
 
+//________________________________________________________________________________________________
+//
+
 pub fn highest_paid_employee(mall: mall::Mall) -> Vec<mall::floor::store::employee::Employee> {
-    mall.floors
+    let total_employees: Vec<&mall::floor::store::employee::Employee> = mall
+        .floors
         .iter()
         .flat_map(|f| f.stores.iter().flat_map(|s| s.employees.iter()))
-        .filter(|e| e.salary >= 1200.0)
+        .collect();
+
+    let highest_salary = total_employees
+        .iter()
+        .map(|e| e.salary)
+        .reduce(f64::max)
+        .unwrap_or(0.0);
+
+    total_employees
+        .into_iter()
+        .filter(|e| e.salary >= highest_salary)
         .cloned()
         .collect::<Vec<mall::floor::store::employee::Employee>>()
 }
+
+//________________________________________________________________________________________________
+//
 
 pub fn nbr_of_employees(mall: mall::Mall) -> usize {
     mall.guards.len()
@@ -31,10 +48,35 @@ pub fn nbr_of_employees(mall: mall::Mall) -> usize {
             .sum::<usize>()
 }
 
-pub fn check_for_securities(mall: &mall::Mall, guards: Vec<mall::guard::Guard>) {
-    while 
+//________________________________________________________________________________________________
+//
+
+pub fn check_for_securities(mall: &mut mall::Mall, mut guards: Vec<mall::guard::Guard>) {
+    let surface: u64 = mall.floors.iter().map(|f| f.size_limit).sum();
+
+    while surface / guards.len() as u64 >= 200 {
+        println!("{:#?}", guards.len());
+        guards.push(guards[0].clone())
+    }
+
+    mall.guards = guards
 }
 
-pub fn cut_or_raise(mall: &mall::Mall) {
-    todo!()
+//________________________________________________________________________________________________
+//
+
+pub fn cut_or_raise(mall: &mut mall::Mall) {
+    mall.floors
+        .iter_mut()
+        .flat_map(|f| f.stores.iter_mut().flat_map(|s| s.employees.iter_mut()))
+        .for_each(|e| {
+            let percent = e.salary * 10.0 / 100.0;
+            let work_time = e.working_hours.1 - e.working_hours.0;
+
+            if work_time < 10 {
+                e.raise((percent * 1000.0).round() / 1000.0);
+            } else {
+                e.cut((percent * 1000.0).round() / 1000.0);
+            }
+        });
 }
