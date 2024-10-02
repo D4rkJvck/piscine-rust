8fd8c3d8878d812b8c4dd5f6cd7f9a29271fe69f
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-10-02 22:06:21 +0000

    CREATES - 5

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -12,7 +12,11 @@ pub fn expected_variable(to_cmp: &str, expected: &str) -> Option<String> {
         expected.to_ascii_lowercase().as_str(),
     );
 
-    let similarity_percentage = (expected.len() - diff) * 100 / expected.len();
+    if diff > expected.len() {
+        return None
+    };
+
+    let similarity_percentage = 100 - (diff * 100 / expected.len());
 
     if similarity_percentage > 50 {
         Some(format!("{}%", similarity_percentage))
