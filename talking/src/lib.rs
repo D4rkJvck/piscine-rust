c969e7222243ca36712e928db9237ab8b2e38fb6
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-10-03 11:28:14 +0000

    PATTERNS - 6

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn talking(text: &str) -> &str {
     match text {
-        s if s.is_empty() => "Just say something!",
+        s if !s.chars().any(|c| c.is_alphabetic()) => "Just say something!",
         s if !s.chars().any(|c| c.is_lowercase()) && s.ends_with('?') => "Quiet, I am thinking!",
         s if s.ends_with('?') => "Sure.",
         s if !s.chars().any(|c| c.is_lowercase()) => "There is no need to yell, calm down!",
