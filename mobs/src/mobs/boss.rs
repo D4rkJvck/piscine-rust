44faaf2d38db5b5f853b93d5ea377c342750afe2
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-10-03 01:52:51 +0000

    CREATES - 6

--- mobs/src/mobs/boss.rs
+++ mobs/src/mobs/boss.rs
@@ -1,20 +1,12 @@
 #[derive(Debug, Clone, PartialEq)]
 pub struct Boss {
-    name: String,
-    age: u8,
+    pub name: String,
+    pub age: u8,
 }
 
 impl Boss {
     pub fn new(n: &str, age: u8) -> Self {
         let name = n.to_owned();
         Boss { name, age }
-    }
-
-    pub fn get_name(&self) -> &str {
-        self.name.as_str()
-    }
-
-    pub fn get_age(&self) -> u8 {
-        self.age
     }
 }
