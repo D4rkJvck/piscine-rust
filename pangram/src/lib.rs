eac963ebf0450fee4ef850ba20016e5da32a3ce3
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-10-03 10:57:24 +0000

    PATTERNS - 5

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,24 +1,11 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub fn is_pangram(string: &str) -> bool {
-    let mut map: HashMap<char, bool> = HashMap::new();
+    let mut set: HashSet<char> = HashSet::new();
 
-    for c in "abcdefghijklmnopqrstuvwxyz".chars() {
-        map.insert(c, false);
+    for c in string.to_lowercase().chars().filter(|c| c.is_alphabetic()) {
+        set.insert(c);
     }
 
-    for c in string
-        .to_lowercase()
-        .chars()
-        .filter(|c| c.is_alphabetic())
-        .collect::<Vec<char>>()
-    {
-        if !map.get(&c).unwrap() {
-            *map.get_mut(&c).unwrap() = true
-        }
-    }
-
-    println!("{:#?}", map);
-
-    map.iter().all(|(_, b)| *b == true)
+    ('a'..='z').all(|c| set.contains(&c))
 }
