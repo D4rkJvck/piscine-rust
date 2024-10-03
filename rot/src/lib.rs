bcf849face82339e80d66d3eeba3d32e6b106f19
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-10-03 19:46:39 +0000

    PATTERNS - 8

---
+++ rot/src/lib.rs
@@ -0,0 +1,31 @@
+pub fn rotate(input: &str, key: i8) -> String {
+    if key < -25 || key > 25 {
+        return input.to_string();
+    };
+
+    let mut ciphered = String::new();
+    let negative = if key < 0 { true } else { false };
+
+    for c in input.chars() {
+        match c {
+            'a'..='z' => {
+                if negative {
+                    ciphered.push((b'z' - (key.abs() as u8 + b'z' - c as u8) % 26) as char);
+                } else {
+                    ciphered.push(((c as u8 - b'a' + key as u8) % 26 + b'a') as char);
+                }
+            },
+            'A'..='Z' => {
+                if negative {
+                    ciphered.push((b'Z' - (key.abs() as u8 + b'Z' - c as u8) % 26) as char);
+                } else {
+                    ciphered.push(((c as u8 - b'A' + key as u8) % 26 + b'A') as char);
+                }
+            },
+            _ => ciphered.push(c),
+        }
+    }
+
+    ciphered
+}
+
