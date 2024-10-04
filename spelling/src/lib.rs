9a9a24d6b8d7432b8a6573849e01695bb1a74fb9
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-10-04 17:44:32 +0000

    PATTERNS - 13

--- spelling/src/lib.rs
+++ spelling/src/lib.rs
@@ -2,11 +2,11 @@ pub fn spell(n: u64) -> String {
     if n == 0 {
         String::from("zero")
     } else {
-        count(n)
+        spelling(n)
     }
 }
 
-fn count(n: u64) -> String {
+fn spelling(n: u64) -> String {
     match n {
         1 => String::from("one"),
         2 => String::from("two"),
@@ -22,13 +22,14 @@ fn count(n: u64) -> String {
         12 => String::from("twelve"),
         13 => String::from("thirteen"),
         15 => String::from("fifteen"),
-        14 | 16 | 17 | 18 | 19 => count(n % 10) + "teen",
+        14 | 16 | 17 | 18 | 19 => spelling(n % 10) + "teen",
+        20 => String::from("twenty"),
         30 => String::from("thirty-"),
         50 => String::from("fifty-"),
-        n if n >= 1_000_000 => count(n / 1_000_000) + " million " + &count(n % 1_000_000),
-        n if n >= 1_000 => count(n / 1_000) + " thousand " + &count(n % 1_000),
-        n if n >= 100 => count(n / 100) + " hundred " + &count(n % 100),
-        n if n > 10 => count(n / 10) + "ty-" + &count(n % 10),
+        n if n >= 1_000_000 => spelling(n / 1_000_000) + " million " + &spelling(n % 1_000_000),
+        n if n >= 1_000 => spelling(n / 1_000) + " thousand " + &spelling(n % 1_000),
+        n if n >= 100 => spelling(n / 100) + " hundred " + &spelling(n % 100),
+        n if n > 10 => spelling(n / 10) + "ty-" + &spelling(n % 10),
         _ => String::from(""),
     }
 }
