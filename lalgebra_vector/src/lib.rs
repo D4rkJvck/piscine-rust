662648c297f42429536dd4f2381f82fa964da809
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-10-11 17:07:07 +0000

    TRAITS - 9

---
+++ lalgebra_vector/src/lib.rs
@@ -0,0 +1,50 @@
+use std::{iter::Sum, ops::Add};
+
+pub use lalgebra_scalar::*;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Vector<T: Scalar>(pub Vec<T>);
+
+impl<T> Add for Vector<T>
+where
+    T: Scalar + Sum + Clone,
+{
+    type Output = Option<Vec<T>>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.0.len() != rhs.0.len() {
+            return None;
+        };
+
+        Some(
+            self.0
+                .iter()
+                .zip(rhs.0.iter())
+                .map(|(a, b)| a.clone() + b.clone())
+                .collect::<Vec<T>>(),
+        )
+    }
+}
+
+impl<T> Vector<T>
+where
+    T: Scalar + Sum + Clone,
+{
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    pub fn dot(&self, other: &Self) -> Option<T> {
+        if self.0.len() != other.0.len() {
+            return None;
+        };
+
+        Some(
+            self.0
+                .iter()
+                .zip(other.0.iter())
+                .map(|(a, b)| a.clone() * b.clone())
+                .sum::<T>(),
+        )
+    }
+}
