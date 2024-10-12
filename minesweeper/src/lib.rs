f4282c304ee4bdcb926f36c4f3ca748c15171ea2
jefaye <clement.jean.l.faye@gmail.com>
2024-10-12 09:16:39 +0000

    TRAITS [DONE]

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -8,41 +8,57 @@ pub fn solve_board(minefield: &[&str]) -> Vec<String> {
             let mut mine_count = 0;
 
             if col == ' ' {
+
+                // IDEA:`Up`
                 if i > 0 {
                     if minefield[i - 1].chars().nth(j).unwrap() == '*' {
                         mine_count += 1;
                     }
                 }
+
+                // IDEA:`Down`
                 if i < minefield.len() - 1 {
                     if minefield[i + 1].chars().nth(j).unwrap() == '*' {
                         mine_count += 1;
                     }
                 }
+
+                // IDEA:`Left`
                 if j > 0 {
                     if minefield[i].chars().nth(j - 1).unwrap() == '*' {
                         mine_count += 1;
                     }
                 }
+
+                // IDEA:`Right`
                 if j < row.len() - 1 {
                     if minefield[i].chars().nth(j + 1).unwrap() == '*' {
                         mine_count += 1;
                     }
                 }
+
+                // IDEA:`Up Left`
                 if i > 0 && j > 0 {
                     if minefield[i -1].chars().nth(j - 1).unwrap() == '*' {
                         mine_count += 1;
                     }
                 }
+
+                // IDEA:`Up Right`
                 if i > 0 && j < row.len() - 1 {
                     if minefield[i - 1].chars().nth(j + 1).unwrap() == '*' {
                         mine_count += 1;
                     }
                 }
+
+                // IDEA:`Down Left`
                 if i < minefield.len() - 1 && j > 0 {
                     if minefield[i + 1].chars().nth(j - 1).unwrap() == '*' {
                         mine_count += 1;
                     }
                 }
+
+                // IDEA:`Down Right`
                 if i < minefield.len() - 1 && j < row.len() - 1 {
                     if minefield[i + 1].chars().nth(j + 1).unwrap() == '*' {
                         mine_count += 1;
@@ -54,6 +70,7 @@ pub fn solve_board(minefield: &[&str]) -> Vec<String> {
                 } else {
                     res_str.push(' ');
                 }
+                
             } else {
                 res_str.push(col);
             }
