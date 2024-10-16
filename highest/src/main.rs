47786d441107a9bbba395af2bb18000450206c01
jefaye <clement.jean.l.faye@gmail.com>
2024-10-16 17:58:46 +0000

    FFEATURES - 6

---
+++ highest/src/main.rs
@@ -0,0 +1,18 @@
+use highest::*;
+
+fn main() {
+    let expected = [30, 500, 20, 70];
+    let n = Numbers::new(&expected);
+    println!("{:?}", n.list());
+    println!("{:?}", n.highest());
+    println!("{:?}", n.latest());
+    println!("{:?}", n.highest_three());
+}
+
+
+// $ cargo run
+// [30, 500, 20, 70]
+// Some(500)
+// Some(70)
+// [500, 70, 30]
+// $
