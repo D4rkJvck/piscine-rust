ed9765ecef926edc23551fe733e1988af845b1e7
jefaye <clement.jean.l.faye@gmail.com>
2024-10-16 20:55:19 +0000

    FEATURES  7

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -8,13 +8,13 @@ impl Iterator for Collatz {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.v = match self.v {
-            0 | 1 => 0,
+            0 | 1 => 1,
             v if v % 2 == 0 => v / 2,
             v => 3 * v + 1,
         };
 
         match self.v {
-            0 => None,
+            1 => None,
             _ => Some(Self::new(self.v)),
         }
     }
@@ -27,12 +27,5 @@ impl Collatz {
 }
 
 pub fn collatz(n: u64) -> usize {
-    let mut count = 0;
-    let mut collatz = Collatz::new(n);
-
-    while let Some(_) = collatz.next() {
-        count += 1;
-    }
-
-    count
+    Collatz::new(n).count()
 }
