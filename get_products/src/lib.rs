a4b2f080fcdf053d39f25145f62109787575e8b2
jefaye <clement.jean.l.faye@gmail.com>
2024-10-16 17:28:34 +0000

    FEATURES - 5

---
+++ get_products/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn get_products(vec: Vec<usize>) -> Vec<usize> {
+    let mut result = vec![];
+
+    for int in vec.iter() {
+        let mut product = 1;
+
+        for other in vec.iter() {
+            if other != int {
+                product *= other
+            }
+        }
+
+        result.push(product);
+    }
+
+    result
+}
