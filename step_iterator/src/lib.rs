c1d5de75797a97ff5e37193b60dd2aa9698b84b0
jefaye <clement.jean.l.faye@gmail.com>
2024-10-17 11:04:38 +0000

    LIFETIMES - 10

---
+++ step_iterator/src/lib.rs
@@ -0,0 +1,32 @@
+use std::{fmt::Display, iter::Iterator, ops::AddAssign};
+
+pub struct StepIterator<T> {
+    beg: T,
+    end: T,
+    steps: T,
+}
+
+impl<T> StepIterator<T> {
+    pub fn new(beg: T, end: T, steps: T) -> Self {
+        StepIterator { beg, end, steps }
+    }
+}
+
+impl<T> Iterator for StepIterator<T>
+where
+    T: AddAssign + PartialOrd + Copy,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.beg > self.end {
+            return None;
+        };
+
+        let next = self.beg;
+
+        self.beg += self.steps;
+
+        Some(next)
+    }
+}
