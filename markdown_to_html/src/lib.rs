c1910af12b6be9b1070224771660d52fd130b5f6
D4rkJvck <122977558+D4rkJvck@users.noreply.github.com>
2024-10-18 19:59:24 +0000

    FEATURES - 12

--- markdown_to_html/src/lib.rs
+++ markdown_to_html/src/lib.rs
@@ -1,3 +1,50 @@
 pub fn markdown_to_html(s: &str) -> String {
-    todo!()
+    let mut html = String::new();
+
+    let lines = s.lines();
+
+    for line in lines {
+        if line.is_empty() {
+            html.push('\n');
+            continue;
+        }
+
+        let words: Vec<&str> = line.split_whitespace().collect();
+
+        if words.len() < 2 {
+            html.push_str(&words[0]);
+            continue;
+        }
+
+        // Convert Headings
+        match words[0] {
+            "#" => html.push_str(&heading(1, &words[1..].join(" "))),
+            "##" => html.push_str(&heading(2, &words[1..].join(" "))),
+            "###" => html.push_str(&heading(3, &words[1..].join(" "))),
+            ">" => html.push_str(
+                format!(
+                    "<{}>{}</{}>\n",
+                    &words[1].to_lowercase(),
+                    &words[1..].join(" "),
+                    &words[1].to_lowercase()
+                )
+                .as_str(),
+            ),
+            _ => html.push_str(format!("{}\n", &words.join(" ")).as_str()),
+        }
+    }
+
+    html = html
+        .split("**")
+        .collect::<Vec<&str>>()
+        .join("<strong>")
+        .split("*")
+        .collect::<Vec<&str>>()
+        .join("<em>");
+
+    html
+}
+
+fn heading(i: usize, text: &str) -> String {
+    format!("<h{}>{}</h{}>\n", i, text, i)
 }
